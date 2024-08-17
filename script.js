539de5a312a49f8f635dd1b95f60a26e5d36d5d6
adarsh40parihar <22je0040@iitism.ac.in>
2024-08-18 02:20:07 +0530

    Js added

---
+++ script.js
@@ -0,0 +1,25 @@
+const speedUp = document.querySelector("#speedup")
+const speedDown = document.querySelector("#speeddown")
+const volumeUp = document.querySelector("#volumeup")
+const volumeDown = document.querySelector("#volumedown")
+const videoBtn = document.querySelector("#videoBtn");
+const videoInput = document.querySelector("#videoInput");
+
+
+const speedUpHandler = function () {
+    console.log("Speed Up was Clicked");
+}
+speedUp.addEventListener("click",speedUpHandler);
+
+const fileSelector = function () {
+    console.log("Open button Clicked");
+    videoInput.click();
+}
+videoBtn.addEventListener("click",fileSelector);
+
+// when file uploaded or selected then change occur
+const acceptInputHandler = (obj) =>{
+    console.log("File uploaded successfully");
+    console.log(obj.target.files);
+}
+videoInput.addEventListener("change",acceptInputHandler);
