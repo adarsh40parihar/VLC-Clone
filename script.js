b98bf54c168d9c50e3fce331bc45f18ac0dc40a1
adarsh40parihar <22je0040@iitism.ac.in>
2024-08-18 22:11:19 +0530

    Some Changes made

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-const speedUp = document.querySelector("#speedup")
-const speedDown = document.querySelector("#speeddown")
-const volumeUp = document.querySelector("#volumeup")
-const volumeDown = document.querySelector("#volumedown")
+/**************************open button************************/
+
 const videoBtn = document.querySelector("#videoBtn");
 const videoInput = document.querySelector("#videoInput");
+const videoPlayer = document.querySelector("#main");
+const defaultImg = document.querySelector("#default-img");
 
-
-const speedUpHandler = function () {
-    console.log("Speed Up was Clicked");
-}
-speedUp.addEventListener("click",speedUpHandler);
 // when open button is clicked
 const fileSelector = function () {
     console.log("Open button Clicked");
     videoInput.click();
 }
-videoBtn.addEventListener("click",fileSelector);
-
 // when file uploaded or selected then change occur
 const acceptInputHandler = (obj) =>{
     console.log("File uploaded successfully");
-    console.log(obj.target.files);
+    const selectedVideo = obj.target.files[0];
+    //first remove the vlc icon
+    defaultImg.style.display = "none";
+    // src => base64
+    const link = URL.createObjectURL(selectedVideo);
+    const videoElement = document.createElement("video");
+    videoElement.src = link;
+    videoElement.setAttribute("class","video");
+    videoElement.controls="true";
+    videoPlayer.appendChild(videoElement);
+    videoElement.play();
 }
+videoBtn.addEventListener("click",fileSelector);
 videoInput.addEventListener("change",acceptInputHandler);
+
+
+   /*******************volume and speed*******************/
+// select the element
+const speedUp = document.querySelector("#speedup")
+const speedDown = document.querySelector("#speeddown")
+const volumeUp = document.querySelector("#volumeup")
+const volumeDown = document.querySelector("#volumedown")
+
+
+
+const speedUpHandler = function () {
+    alert("Speed Up was Clicked");
+}
+speedUp.addEventListener("click",speedUpHandler);
