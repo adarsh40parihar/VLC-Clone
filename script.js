7bf835b86af976ea21c046af811fcfe3c198837d
adarsh40parihar <22je0040@iitism.ac.in>
2024-08-22 00:35:24 +0530

    toast added

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ const acceptInputHandler = (obj) =>{
     const videoElement = document.createElement("video");
     videoElement.src = link;
     videoElement.setAttribute("class","video");
-    videoElement.controls="true";
     videoPlayer.appendChild(videoElement);
     videoElement.play();
 }
@@ -35,10 +34,89 @@ const speedUp = document.querySelector("#speedup")
 const speedDown = document.querySelector("#speeddown")
 const volumeUp = document.querySelector("#volumeup")
 const volumeDown = document.querySelector("#volumedown")
+const mute = document.querySelector("#mute")
+const toast = document.querySelector("#toast")
 
 
+const showToast = (obj) =>{
+    toast.textContent = obj;
+    toast.style.display = "block";
+    setTimeout(() => {
+        toast.style.display = "none";
+    },2000);
+}
 
 const speedUpHandler = function () {
-    alert("Speed Up was Clicked");
+    const videoElement = document.querySelector("video");
+    if(videoElement==null){
+        return;
+    }
+    if (videoElement.playbackRate>=3){
+        return;
+    }
+    videoElement.playbackRate = videoElement.playbackRate + 0.5;
+    
+    showToast(videoElement.playbackRate + "X");
+
+    console.log(videoElement.playbackRate);
+}
+const speedDownHandler = function () {
+    const videoElement = document.querySelector("video");
+    if(videoElement==null){
+        return;
+    }
+    if (videoElement.playbackRate<=0.5){
+        return;
+    }
+    videoElement.playbackRate = videoElement.playbackRate - 0.5;
+   showToast(videoElement.playbackRate + "X");
+
+    console.log(videoElement.playbackRate);
+}
+const volumeUpHandler = function () {
+    const videoElement = document.querySelector("video");
+    if(videoElement==null){
+        return;
+    }
+    if (videoElement.volume>=0.99){
+        showToast("100%");
+        return;
+    }
+    videoElement.volume = videoElement.volume + 0.1;
+    showToast(Math.round(videoElement.volume*100) + "%");
+    console.log(videoElement.volume);
+}
+const volumeDownHandler = function () {
+    const videoElement = document.querySelector("video");
+    if(videoElement==null){
+        return;
+    }
+    if (videoElement.volume <= 0.1) {
+        showToast("0%");
+        videoElement.volume = 0;
+        return;
+    }
+    videoElement.volume = videoElement.volume - 0.1;
+    showToast(Math.round(videoElement.volume*100) + "%");
+    console.log(videoElement.volume);
+}
+const volumeMute = function () {
+    console.log("mute");
+    const videoElement = document.querySelector("video");
+    if(videoElement==null){
+        return;
+    }
+    else if (videoElement.volume<=0){
+        videoElement.volume = 0.5;
+        return;
+    }
+
+    videoElement.volume = 0;
+    showToast(videoElement.volume*100 + "%");
+    console.log(videoElement.volume);
 }
 speedUp.addEventListener("click",speedUpHandler);
+speedDown.addEventListener("click",speedDownHandler);
+volumeUp.addEventListener("click",volumeUpHandler);
+volumeDown.addEventListener("click",volumeDownHandler);
+mute.addEventListener("click",volumeMute);
